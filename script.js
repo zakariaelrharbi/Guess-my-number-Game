bc6de447be116cbd349c057c493d9eda8918ac91
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2026-01-28 19:14:51 +0100

    html and css added

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict'
+
+
+// document.querySelector('.message').textContent = '🎉 Correct Number!'
+
+// document.querySelector('.number').textContent = '13'
+// document.querySelector('.score').textContent = '10'
+// document.querySelector('.guess').value= 30
+// console.log(document.querySelector('.guess').value);
+
+
+

e46e750d292e1e8904effe6958341630d88eb659
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2026-01-31 16:13:53 +0100

    guess my number game done

--- script.js
+++ script.js
@@ -8,5 +8,54 @@
 // document.querySelector('.guess').value= 30
 // console.log(document.querySelector('.guess').value);
 
+const secretNumber = Math.trunc(Math.random()*20) + 1
+let score = 20;
+let highscore = 0;
 
 
+document.querySelector('.check').addEventListener
+('click', function(){
+    const guess = document.querySelector('.guess').value
+    
+
+    if(!guess){
+        document.querySelector('.message').textContent = '⛔ no number !'
+    }
+    else if (guess == secretNumber) {
+        document.querySelector('.message').textContent = '🎉 Correct Number!'
+        document.querySelector('.number').textContent = secretNumber
+        document.querySelector('body').style.backgroundColor = '#60b347'
+        if(score > highscore){
+            highscore = score
+            document.querySelector('.highscore').textContent = highscore
+        }
+    }
+    else if (guess > secretNumber) {
+        if (score > 0) {
+            document.querySelector('.message').textContent = 'too High! '
+            score = score -1
+            document.querySelector('.score').textContent = score    
+        } else {
+             document.querySelector('.message').textContent = 'You Lost the Game'
+        }
+    }
+    else if (guess < secretNumber) {
+        if (score > 0) {
+            document.querySelector('.message').textContent = 'too Low! '
+            score = score -1
+            document.querySelector('.score').textContent = score    
+        } else {
+            document.querySelector('.message').textContent = 'You Lost the Game'
+        }
+    }
+})
+
+document.querySelector('.again').addEventListener('click', function(){
+    document.querySelector('body').style.backgroundColor = '#222'
+    document.querySelector('.score').textContent = '20'
+    document.querySelector('.message').textContent = 'Start guessing...'
+    document.querySelector('.guess').value = ' '
+    document.querySelector('.number').textContent = '?'
+    document.querySelector('.highscore').textContent = '0'
+
+})
